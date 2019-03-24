45fd9e982c1d4f68b6a66e1ffb177bb6070b1cfb
T-Applesmith <sgf3211@gmail.com>
2019-03-24 18:17:39 -0500

    Condensed config_loaders

--- loader_functions/config_loaders.py
+++ loader_functions/config_loaders.py
@@ -10,136 +10,65 @@ def write_config(config_dict):
 
     # Options
     config['OPTIONS'] = {}
-    
-    if config_dict.get('fps_cap'):
-        config['OPTIONS']['fps_cap'] = config_dict['fps_cap']
-    else:
-        config['OPTIONS']['fps_cap'] = '60'
-        
-    if config_dict.get('fps_display'):
-        config['OPTIONS']['fps_display'] = config_dict['fps_display']
-    else:
-        config['OPTIONS']['fps_display'] = 'False'
+
+    setup_options_config(config, config_dict, 'OPTIONS', 'fps_cap', '60')
+    setup_options_config(config, config_dict, 'OPTIONS', 'fps_display', 'False')
 
     # Keybindings
     config['KEYBINDINGS'] = {}
 
     # Keybinding - Movement
-    if config_dict.get('key_wait') and (config_dict['allow_keybinding'] in ['True', 'TRUE']):
-        config['KEYBINDINGS']['key_wait'] = config_dict['key_wait']
-    else:
-        config['KEYBINDINGS']['key_wait'] = 'z'
-
-    if config_dict.get('key_north') and (config_dict['allow_keybinding'] in ['True', 'TRUE']):
-        config['KEYBINDINGS']['key_north'] = config_dict['key_north']
-    else:
-        config['KEYBINDINGS']['key_north'] = 'k'
-
-    if config_dict.get('key_south') and (config_dict['allow_keybinding'] in ['True', 'TRUE']):
-        config['KEYBINDINGS']['key_south'] = config_dict['key_south']
-    else:
-        config['KEYBINDINGS']['key_south'] = 'j'
-
-    if config_dict.get('key_west') and (config_dict['allow_keybinding'] in ['True', 'TRUE']):
-        config['KEYBINDINGS']['key_west'] = config_dict['key_west']
-    else:
-        config['KEYBINDINGS']['key_west'] = 'h'
-
-    if config_dict.get('key_east') and (config_dict['allow_keybinding'] in ['True', 'TRUE']):
-        config['KEYBINDINGS']['key_east'] = config_dict['key_east']
-    else:
-        config['KEYBINDINGS']['key_east'] = 'l'
-
-    if config_dict.get('key_northwest') and (config_dict['allow_keybinding'] in ['True', 'TRUE']):
-        config['KEYBINDINGS']['key_northwest'] = config_dict['key_northwest']
-    else:
-        config['KEYBINDINGS']['key_northwest'] = 'y'
-
-    if config_dict.get('key_northeast') and (config_dict['allow_keybinding'] in ['True', 'TRUE']):
-        config['KEYBINDINGS']['key_northeast'] = config_dict['key_northeast']
-    else:
-        config['KEYBINDINGS']['key_northeast'] = 'u'
-
-    if config_dict.get('key_southwest') and (config_dict['allow_keybinding'] in ['True', 'TRUE']):
-        config['KEYBINDINGS']['key_southwest'] = config_dict['key_southwest']
-    else:
-        config['KEYBINDINGS']['key_southwest'] = 'b'
-
-    if config_dict.get('key_southeast') and (config_dict['allow_keybinding'] in ['True', 'TRUE']):
-        config['KEYBINDINGS']['key_southeast'] = config_dict['key_southeast']
-    else:
-        config['KEYBINDINGS']['key_southeast'] = 'n'
-
-    if config_dict.get('key_down_stairs') and (config_dict['allow_keybinding'] in ['True', 'TRUE']):
-        config['KEYBINDINGS']['key_down_stairs'] = config_dict['key_down_stairs']
-    else:
-        config['KEYBINDINGS']['key_down_stairs'] = '>'
-
-    if config_dict.get('key_up_stairs') and (config_dict['allow_keybinding'] in ['True', 'TRUE']):
-        config['KEYBINDINGS']['key_up_stairs'] = config_dict['key_up_stairs']
-    else:
-        config['KEYBINDINGS']['key_up_stairs'] = '<'
+    setup_keybindings_config(config, config_dict, 'KEYBINDINGS', 'key_wait', 'z')
+    setup_keybindings_config(config, config_dict, 'KEYBINDINGS', 'key_north', 'k')
+    setup_keybindings_config(config, config_dict, 'KEYBINDINGS', 'key_south', 'j')
+    setup_keybindings_config(config, config_dict, 'KEYBINDINGS', 'key_west', 'h')
+    setup_keybindings_config(config, config_dict, 'KEYBINDINGS', 'key_east', 'l')
+    setup_keybindings_config(config, config_dict, 'KEYBINDINGS', 'key_northwest', 'y')
+    setup_keybindings_config(config, config_dict, 'KEYBINDINGS', 'key_northeast', 'u')
+    setup_keybindings_config(config, config_dict, 'KEYBINDINGS', 'key_southwest', 'b')
+    setup_keybindings_config(config, config_dict, 'KEYBINDINGS', 'key_southeast', 'n')
+    setup_keybindings_config(config, config_dict, 'KEYBINDINGS', 'key_down_stairs', '>')
+    setup_keybindings_config(config, config_dict, 'KEYBINDINGS', 'key_up_stairs', '<')
+    setup_keybindings_config(config, config_dict, 'KEYBINDINGS', 'key_west', 'h')
 
     # Keybinding - Actions
-    if config_dict.get('key_pickup') and (config_dict['allow_keybinding'] in ['True', 'TRUE']):
-        config['KEYBINDINGS']['key_pickup'] = config_dict['key_pickup']
-    else:
-        config['KEYBINDINGS']['key_pickup'] = 'g'
-
-    if config_dict.get('key_drop') and (config_dict['allow_keybinding'] in ['True', 'TRUE']):
-        config['KEYBINDINGS']['key_drop'] = config_dict['key_drop']
-    else:
-        config['KEYBINDINGS']['key_drop'] = 'd'
-
-    if config_dict.get('key_look') and (config_dict['allow_keybinding'] in ['True', 'TRUE']):
-        config['KEYBINDINGS']['key_look'] = config_dict['key_look']
-    else:
-        config['KEYBINDINGS']['key_look'] = 'L'
+    setup_keybindings_config(config, config_dict, 'KEYBINDINGS', 'key_pickup', 'g')
+    setup_keybindings_config(config, config_dict, 'KEYBINDINGS', 'key_drop', 'd')
+    setup_keybindings_config(config, config_dict, 'KEYBINDINGS', 'key_look', 'L')
 
     # Keybinding - Menus
-    if config_dict.get('key_inventory') and (config_dict['allow_keybinding'] in ['True', 'TRUE']):
-        config['KEYBINDINGS']['key_inventory'] = config_dict['key_inventory']
-    else:
-        config['KEYBINDINGS']['key_inventory'] = 'i'
-
-    if config_dict.get('key_character_menu') and (config_dict['allow_keybinding'] in ['True', 'TRUE']):
-        config['KEYBINDINGS']['key_character_menu'] = config_dict['key_character_menu']
-    else:
-        config['KEYBINDINGS']['key_character_menu'] = 'c'
-
-    if config_dict.get('key_equipment') and (config_dict['allow_keybinding'] in ['True', 'TRUE']):
-        config['KEYBINDINGS']['key_equipment'] = config_dict['key_equipment']
-    else:
-        config['KEYBINDINGS']['key_equipment'] = 'e'
-
-    if config_dict.get('key_help') and (config_dict['allow_keybinding'] in ['True', 'TRUE']):
-        config['KEYBINDINGS']['key_help'] = config_dict['key_help']
-    else:
-        config['KEYBINDINGS']['key_help'] = '?'
-
-    if config_dict.get('key_dev_console') and (config_dict['allow_keybinding'] in ['True', 'TRUE']):
-        config['KEYBINDINGS']['key_dev_console'] = config_dict['key_dev_console']
-    else:
-        config['KEYBINDINGS']['key_dev_console'] = '~'
+    setup_keybindings_config(config, config_dict, 'KEYBINDINGS', 'key_inventory', 'i')
+    setup_keybindings_config(config, config_dict, 'KEYBINDINGS', 'key_character_menu', 'c')
+    setup_keybindings_config(config, config_dict, 'KEYBINDINGS', 'key_equipment', 'e')
+    setup_keybindings_config(config, config_dict, 'KEYBINDINGS', 'key_help', '?')
+    setup_keybindings_config(config, config_dict, 'KEYBINDINGS', 'key_dev_console', '~')
       
     # Development Tools
     config['DEV'] = {}
 
-    if config_dict.get('allow_keybinding'):
-        config['DEV']['allow_keybinding'] = config_dict['allow_keybinding']
-    else:
-        config['DEV']['allow_keybinding'] = 'False'
-
-    if config_dict.get('verbose'):
-        config['DEV']['verbose'] = config_dict['verbose']
-    else:
-        config['DEV']['verbose'] = 'False'
+    setup_options_config(config, config_dict, 'DEV', 'allow_keybinding', 'False')
+    setup_options_config(config, config_dict, 'DEV', 'verbose', 'False')
         
     # write to file
     with open('config.ini', 'w') as configfile:
         config.write(configfile)
 
     return
+
+
+def setup_options_config(config, config_dict, location, name, default):
+    if config_dict.get(name):
+        config[location][name] = config_dict[name]
+    else:
+        config[location][name] = default
+    return
+
+
+def setup_keybindings_config(config, config_dict, location, name, default):
+    if config_dict.get(name) and config_dict['allow_keybinding'].lower == 'true':
+        config[location][name] = config_dict[name]
+    else:
+        config[location][name] = default
 
 
 def read_config():
